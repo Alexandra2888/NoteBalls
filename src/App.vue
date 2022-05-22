f5d2ee076a3402826aa053def40a806ac113c88e
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-05-22 15:04:56 +0300

    adding bulma

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
-
-
 <template>
-  <RouterLink to="/">Notes</RouterLink>
-  <RouterLink to="/stats">Stats</RouterLink>
-<RouterView />
+  <NavBar />
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
 
+<script setup>
+/*
+  imports
+*/
+import NavBar from './components/Layout/Navbar.vue'
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
